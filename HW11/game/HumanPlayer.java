0e71828e9ef72baf483e6ef943e1f81d3a15067f
Andrew Shustrov <a.shustrov5@gmail.com>
2023-11-22 22:38:48 +0300

    base game finished?

--- HW11/game/HumanPlayer.java
+++ HW11/game/HumanPlayer.java
@@ -1,14 +1,12 @@
 package game;
 
 import java.io.PrintStream;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
- */
 public class HumanPlayer implements Player {
     private final PrintStream out;
-    private final Scanner in;
+    private Scanner in;
 
     public HumanPlayer(final PrintStream out, final Scanner in) {
         this.out = out;
@@ -22,17 +20,20 @@ public class HumanPlayer implements Player {
     @Override
     public Move move(final Position position, final Cell cell) {
         while (true) {
-            out.println("Position");
-            out.println(position);
-            out.println(cell + "'s move");
-            out.println("Enter row and column");
-            final Move move = new Move(in.nextInt(), in.nextInt(), cell);
-            if (position.isValid(move)) {
-                return move;
+            try {
+                this.in = new Scanner(System.in);
+                out.println("Position");
+                out.println(position);
+                out.println(cell + "'s move");
+                out.println("Enter row and column: ");
+                final Move move = new Move(in.nextInt(), in.nextInt(), cell);
+                if (position.isValid(move)) {
+                    return move;
+                }
+                out.println("Move " + move + " is invalid");
+            } catch (InputMismatchException e) {
+                System.out.println("You have not entered the row and column numbers.");
             }
-            final int row = move.getRow();
-            final int column = move.getColumn();
-            out.println("Move " + move + " is invalid");
         }
     }
 }
